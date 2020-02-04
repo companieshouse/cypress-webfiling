39a579d3a2bf1d6ae109e3a9491cc38bdfa4171c
danfalc <dfalconer@companieshouse.gov.uk>
2020-02-04 10:38:10 +0000

    Update CorporateOfficerAppointment.js

--- cypress/support/page_objects/generic/CorporateOfficerAppointment.js
+++ cypress/support/page_objects/generic/CorporateOfficerAppointment.js
@@ -22,7 +22,7 @@ class CorporateOfficerAppointment extends BasePage {
     enterCompanyAddress(propertyNumber, postcode) {
         addressPage.lookUpServiceAddress(propertyNumber, postcode);
         // Accessibility check here
-        //cy.accessibilityCheck();
+        cy.accessibilityCheck();
         cy.get('#company-address-container-continue').wait(2000).click();
     }
 
