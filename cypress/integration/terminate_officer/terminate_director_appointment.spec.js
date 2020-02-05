689e3e2b0d903b657b5164dd18571189ff0877e2
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2020-02-05 11:47:55 +0000

    Add TM01 test with error validation

---
+++ cypress/integration/terminate_officer/terminate_director_appointment.spec.js
@@ -0,0 +1,36 @@
+import CompanyOverviewPage from '../../support/page_objects/CompanyOverviewPage'
+import AllFormsPage from '../../support/page_objects/AllformsPage'
+import DirectorAndSecretariesPage from '../../support/page_objects/DirectorsAndSecretariesPage'
+
+const companyOverview = new CompanyOverviewPage();
+const allForms = new AllFormsPage();
+const directorAndSecretaries = new DirectorAndSecretariesPage();
+
+describe('Terminate director appointment - TM01', () => {
+    beforeEach(() => {    
+        // Select form overview
+        companyOverview.selectAllForms();
+        allForms.selectDirectorAndSecretaries()
+            .selectTM01();
+        // Select officer by name
+        cy.accessibilityCheck();
+        directorAndSecretaries.selectOfficerToRemove('Commission Flanker BAROMETERLIQUOR');
+    })
+
+    it('Fill in all fields prior to submission and check accessibility', () => {
+        // Apply today's date for date of change
+        cy.selectTodaysDate("#day-select-1", "#month-select-1", "#year-select-1");
+        // Click the confirm termination checkbox
+        directorAndSecretaries.confirmTermination();
+        // Check accessibility after making changes
+        cy.accessibilityCheck();
+    })
+    
+    it('Submit form with invalid information and check accessibility', () => {
+        // Submit without entering any information to fire errors
+        directorAndSecretaries.submitTermination();
+        // Check accessibility after making changes
+        cy.accessibilityCheck();
+    })
+
+})
