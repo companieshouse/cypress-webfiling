a27496afaa7518221dcfe865ab8a89b2755a03db
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2020-02-07 11:09:44 +0000

    Join PROOF screen check

--- cypress/integration/miscellaneous_screens/check_additional_screens.spec.js
+++ cypress/integration/miscellaneous_screens/check_additional_screens.spec.js
@@ -24,11 +24,21 @@ describe("Miscellaneous other activities that aren't the filing of forms", () =>
         cy.visit('https://ewf-kermit.companieshouse.gov.uk//profile')
     })
 
-    /*it('Join PROOF', () => {
+    it.only('Join PROOF', () => {
+        companyOverview.selectLinkWithText('Join PROOF');
+        cy.accessibilityCheck();
+
+        // Open PROOF t's & c's link. Opens in a new window so call cy.visit() in order to test
+        cy.visit('https://ewf-kermit.companieshouse.gov.uk/help//en/stdwf/proofTandC.html');
+        cy.accessibilityCheck();
+
+        // Go back to company overview screen to exit the test
+        cy.visit('https://ewf-kermit.companieshouse.gov.uk//profile')
+
         
     })
 
-    it('Request Email', () => {
+    /*it('Request Email', () => {
         
     })
 
