8decdd7b43faf6e7f4dc715625526b532f52da95
Daniel Falconer <dfalconer@companieshouse.gov.uk>
2020-03-27 16:16:34 +0000

    remove unnecessary whitespace

--- cypress/support/page_objects/AllformsPage.js
+++ cypress/support/page_objects/AllformsPage.js
@@ -15,7 +15,6 @@ class AllFormsPage {
 
     selectFullAccountsWithAbbreviatedOption() {
         cy.contains('Audit exempt full accounts (with abbreviated accounts option)').click();
-
     }
 
     selectDirectorAndSecretaries() {
